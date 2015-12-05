9da6cdecbb55dc3696fc2c94d47d193f4cf5e9c2
Guido Günther <agx@sigxcpu.org>
2015-12-05 18:14:12 +0100

    Add blog-insert-debian-bts-markdown-link
    This allows to easily link to Debian's BTS

--- blog.el
+++ blog.el
@@ -15,3 +15,21 @@
 	 )
     (when bounds (delete-region (car bounds) (cdr bounds)))
     (markdown-insert-reference-link text "" url "")))
+
+(defun blog-insert-debian-bts-markdown-link ()
+  "Read CVE/DSA number and create link to security tracker"
+  (interactive)
+  (let* (
+         (bounds (or (and (markdown-use-region-p)
+                          (cons (region-beginning) (region-end)))
+                     (markdown-bounds-of-thing-at-point 'symbol)))
+         (text (if bounds
+                   (buffer-substring (car bounds) (cdr bounds))
+                 (read-string "Bugnumber Number: ")))
+	 (bugnum (if (string-prefix-p "#" text)
+		     (substring text 1)
+		 text))
+         (url (concat "http://bugs.debian.org/" bugnum))
+	 )
+    (when bounds (delete-region (car bounds) (cdr bounds)))
+    (markdown-insert-reference-link text "" url "")))
