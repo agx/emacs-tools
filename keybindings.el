d9b279f0aadbaf2091309beeaa145bcfa1e3db3a
Guido Günther <agx@sigxcpu.org>
2015-12-26 14:34:09 +0100

    Add some global keybindings

---
+++ keybindings.el
@@ -0,0 +1,5 @@
+(global-set-key (kbd "C-, c") 'comment-region)
+(global-set-key (kbd "C-, u") 'uncomment-region)
+(global-set-key (kbd "C-, w") 'delete-trailing-whitespace)
+(global-set-key (kbd "C-, r") 'revert-buffer)
+
