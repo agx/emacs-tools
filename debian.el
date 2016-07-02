5969138df724facd88c94d55d485ce9b7e2a3052
Guido Günther <agx@sigxcpu.org>
2016-07-02 19:56:44 +0200

    Add more open-url helpers
    TODO: try to guess URL to use from symbol

--- debian.el
+++ debian.el
@@ -1,12 +1,38 @@
-(defun xcpu-deb-open-bug ()
+(defun xcpu-deb-open-bts ()
   "Open the bug under the cursor in the debian bts"
   (interactive)
   (let* (
          (bounds (bounds-of-thing-at-point 'symbol))
          (text (if bounds
                    (buffer-substring (car bounds) (cdr bounds))
-                 (read-string "Debian Number: ")))
+                 (read-string "Debian Bug Number: ")))
          (url (concat "http://bugs.debian.org/" text))
+	 )
+    (browse-url url)
+    ))
+
+(defun xcpu-deb-open-security-tracker ()
+  "Open the symbol under the cursor in the debian security tracker"
+  (interactive)
+  (let* (
+         (bounds (bounds-of-thing-at-point 'symbol))
+         (text (if bounds
+                   (buffer-substring (car bounds) (cdr bounds))
+                 (read-string "CVE Number: ")))
+         (url (concat "http://security-tracker.debian.org/" text))
+	 )
+    (browse-url url)
+    ))
+
+(defun xcpu-deb-open-tracker ()
+  "Open the symbol under the cursor in the debian package tracker"
+  (interactive)
+  (let* (
+         (bounds (bounds-of-thing-at-point 'symbol))
+         (text (if bounds
+                   (buffer-substring (car bounds) (cdr bounds))
+                 (read-string "Package name: ")))
+         (url (concat "http://tracker.debian.org/pkg/" text))
 	 )
     (browse-url url)
     ))
