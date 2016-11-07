e63d4fd448ca93d5c484932396142571afa41b0a
Guido Günther <agx@sigxcpu.org>
2016-11-07 20:23:09 +0100

    Simplify patch handling

--- keybindings.el
+++ keybindings.el
@@ -4,6 +4,12 @@
   (magit-commit)
   )
 
+(defun xcpu-thank-ack ()
+  (interactive)
+  (search-forward "> ACK")
+  (insert "\n\nApplied. Thanks\n -- Guido\n")
+  (backward-char 4))
+
 (global-set-key (kbd "C-, c") 'comment-region)
 (global-set-key (kbd "C-, u") 'uncomment-region)
 (global-set-key (kbd "C-, w") 'delete-trailing-whitespace)
@@ -16,4 +22,9 @@
 (global-set-key (kbd "C-, C") 'blog-insert-cve-markdown-link)
 (global-set-key (kbd "C-, D") 'blog-insert-debian-bts-markdown-link)
 (global-set-key (kbd "C-, g") 'vc-git-grep)
-(global-set-key (kbd "C-, b") 'xcpu-deb-open-bug)
+
+(global-set-key (kbd "C-, B") 'xcpu-deb-open-bts)
+(global-set-key (kbd "C-, T") 'xcpu-deb-open-tracker)
+(global-set-key (kbd "C-, S") 'xcpu-deb-open-security-tracker)
+
+(global-set-key (kbd "C-< C-<") 'xcpu-thank-ack)
